22bc9bdb5dd8c4744db57e48d12ec432a0891bc3
lyd0 <1170507875@qq.com>
2019-05-25 21:15:03 +0800

    版本有问题

--- src/main/java/com/paper/controller/GenerateController.java
+++ src/main/java/com/paper/controller/GenerateController.java
@@ -1,4 +1,22 @@
 package com.paper.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class GenerateController {
+
+//    到首页
+    @GetMapping("/")
+    public String toIndexPage() {
+        return "index";
+    }
+
+//  到生成页面
+    @GetMapping("/generate")
+    public String toGeneratePage() {
+        return "generate";
+    }
+
+
 }
